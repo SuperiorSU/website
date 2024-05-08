9a4e3d03111fb211a1b722b578f35180543977e3
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-05-08 17:14:39 +0530

    hero done with animations

--- src/app/components/Motionframe.jsx
+++ src/app/components/Motionframe.jsx
@@ -2,6 +2,10 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "../Style/motion.css"
+import "../Style/hero.css"
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import {
   slideInFromLeft,
   slideInFromRight,
@@ -11,8 +15,7 @@ import {
   onLeft,
   onRight,
 } from "@/app/utils/motion";
-import Image from "next/image";
-// import "@/app/Style/hero.css"; 
+import Image from "next/image"; 
 
 const Motionframe = () => {
   // Check if the screen width is medium or larger
@@ -20,13 +23,15 @@ const Motionframe = () => {
   const isSmalllaptop = typeof window !== "undefined" && window.innerWidth >= 1024;
   const isLargeScreen = typeof window !== "undefined" && window.innerWidth >= 1440;
   const smDevices = typeof window !== "undefined" && window.innerWidth >= 640;
-  const isMobile = typeof window !== "undefined" && window.innerWidth <= 640;
+  // const isMobile = typeof window !== "undefined" && window.innerWidth <= 640;
+  const isMobile = typeof window !== "undefined" && window.innerWidth <= 450;
+
+  useEffect(() => {
+    Aos.init();
+  });
 
   return (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      className=" px-5 md:px-20 h-full w-full z-[20]"
+    <div
     >
       {/* in the above motion.div flex flex-col md:flex-row items-center justify-between */}
       {/* <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
@@ -98,32 +103,81 @@ const Motionframe = () => {
       </div> */}
       <div className="grid lg:grid-cols-12 md:grid-cols-12 sm:grid-cols-12 grid-cols-12 place-items-center ">
         <div className=" p-2 lg:col-span-1 md:col-span-1 sm:col-span-1 col-span-1">
-          <Image src="/assets/left_flower.png" alt="" width={700} height={2000} className="mt-6 mr-56"/>
+          <Image src="/assets/left_flower.png" alt="" width={700} height={2000} className="left-flower mt-6 mr-56"
+          data-aos="fade-right"
+          data-aos-delay="20"
+          data-aos-duration="800"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-offset="5"
+          data-aos-anchor-placement="top-left"
+          />
         </div>
         <div className=" p-2 lg:col-span-4 md:col-span-4 sm:col-span-4 col-span-4">
-          <Image src="/assets/man.png" alt="work icons" height={isLargeScreen?2000:1500} width={1000} className=" man  mt-6"/>
+          <Image src="/assets/man.png" alt="work icons" height={isLargeScreen?2000:1500} width={1000} className=" man  mt-6" data-aos="fade-right"
+                data-aos-delay="20"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-offset="5"
+                data-aos-anchor-placement="top-left"/>
         </div>
-        <div className=" p-2 lg:col-span-2 md:lg:col-span-2 sm:col-span-2 col-span-2">
+        <div className=" p-2 w-[300px] lg:col-span-2 md:lg:col-span-2 sm:col-span-2 col-span-2">
           
-            
-            <div className={`font-EvaMayasari font-bold flex justify-center items-center lg:text-[3rem] text-[20px] text-[#094C3B]`}>
-              {/* <div>
-                <Image src="/assets/logo1.png" alt="work icons" height={200} width={200} className="logo" />
-              </div> */}
+        <Image
+            src="/assets/logo1.png"
+            width={isMobile?70:200}
+            height={isMobile?70:200}
+            className="hero-logo mx-auto"
+            alt="Anahata Connections Logo"
+            data-aos="zoom-in-right"
+            data-aos-delay="40"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-anchor-placement="top-left"
+            data-offset="200"
+          />
+            <div className={` anahata font-EvaMayasari font-bold flex justify-center items-center lg:text-[3rem] text-[20px] text-[#094C3B]`}  data-aos="fade-up"
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true">
+           
               Anahata&nbsp;Connections
               
               </div>
-            <div className="font-cantarell flex justify-center mb-10 items-center font-bold lg:text-lg md:text-[11px] text-[12px] text-[#094C3B]">Where Yogic love Blossoms</div>
-            <div className="flex justify-center items-center text-center mt-5 "><button className=" bg-[#094C3B] meet-button  text-white rounded-[32px]">Meet Your Match Now</button></div>
+            <div className="logic-blossoms font-cantarell flex justify-center mb-10 items-center font-bold md:text-lg  text-[12px] text-[#094C3B]"  data-aos="fade-up"
+                data-aos-delay="100"
+                data-aos-duration="1000"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true">Where Yogic love Blossoms</div>
+            <div className="flex justify-center items-center text-center mt-5 "><button className=" bg-[#094C3B] meet-button  text-white rounded-[32px]"  
+               >Meet Your Match Now</button></div>
         </div>
-        <div className=" p-2 lg:col-span-4 md:col-span-4 sm:col-span-4 col-span-4">
-          <Image src="/assets/women.png" alt="work icons" height={2000} width={1000} className="women mt-8 md:me-0 z-[10]"/>
+        <div className=" p-2 lg:col-span-4 md:col-span-4 sm:col-span-4 col-span-4" >
+          <Image src="/assets/women.png" alt="work icons" height={2000} width={1000} className="women mt-8 md:me-0 z-[10]" 
+                data-aos="fade-left"
+                data-aos-delay="20"
+                data-aos-duration="800"
+                data-aos-easing="ease-in-out"
+                data-aos-mirror="true"
+                data-aos-offset="5"
+                data-aos-anchor-placement="top-left"/>
         </div>
         <div className="p-2 lg:col-span-1 md:col-span-1 sm:col-span-1 col-span-1">
-          <Image src="/assets/left_flower.png" alt="" width={700} height={2000} className="mt-6 mr-56 tranform scale-x-[-1] z-[-2]"/>
+          <Image src="/assets/right_flower.png" alt="" width={700} height={2000} className="right-flower mt-6 mr-56  z-[1]"
+              data-aos="fade-left"
+              data-aos-delay="20"
+              data-aos-duration="800"
+              data-aos-easing="ease-in-out"
+              data-aos-mirror="true"
+              data-aos-offset="5"
+              data-aos-anchor-placement="top-left"
+          />
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
